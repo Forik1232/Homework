ef491b833697981175b3b4ae8ba5a54015ce77a1
Daniil <9414354@mail.ru>
2022-04-05 00:44:30 +0300

    conflict conflict

--- src/com/google/Main.java
+++ src/com/google/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello master");
         System.out.println("hello MASTER conflict");
+        System.out.println("hello MASTER conflict");
     }
 }
